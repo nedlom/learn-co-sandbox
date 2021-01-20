33d7075fa84f07ea35fe958f6fad90d3b2a7fd02
imperative-scientist-4600 <nelom3@gmail.com>
2021-01-20 19:55:11 +0000

    initial commit

---
+++ ri_destinations/lib/ri_destinations.rb
@@ -0,0 +1,6 @@
+require "ri_destinations/version"
+
+module RiDestinations
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ ri_destinations/lib/ri_destinations/version.rb
@@ -0,0 +1,3 @@
+module RiDestinations
+  VERSION = "0.1.0"
+end
